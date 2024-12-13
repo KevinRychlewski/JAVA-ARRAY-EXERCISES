49609c4e668a3f4c8413160065f556817eb34e92
kevin <kevinsipioni13@gmail.com>
2024-12-13 20:59:36 -0300

    ArrayExercise

--- src/arrayExercises/ARRAY_EXERCISE3.java
+++ src/arrayExercises/ARRAY_EXERCISE3.java
@@ -1,13 +1,14 @@
 package arrayExercises;
 
 public class ARRAY_EXERCISE3 {
-        public static void main(String[] args) {
-            int[] numeros = {2,4,6,8};
-            int pares = 0;
-            for (int i = 0; i < numeros.length ; i++) {
-                if (numeros[i] %2 == 0);
-                pares++;
-                System.out.println(pares);
+    public static void main(String[] args) {
+        int[] nums = {2, 2, 2, 3};
+        int contador = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                contador++;
             }
         }
+        System.out.println(contador);
+    }
 }
